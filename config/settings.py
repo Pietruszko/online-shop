b60edef3068b73e5c0c938380c0314564b73271c
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-17 16:19:26 +0200

    config: add media url and media root to settings, add serving media through development server when in DEBUG mode

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom settings
+
+# Media
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
