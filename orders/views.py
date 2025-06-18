f1aab85188ddc2586f3370bb8421ae134164953a
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-18 21:45:53 +0200

    feat: create task for celery to send emails after creating an order and add it to order create view

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 def order_create(request):
     cart = Cart(request)
@@ -18,6 +19,8 @@ def order_create(request):
                 )
             # clear the cart
             cart.clear()
+            # Launch asynchronous task
+            order_created.delay(order.id)
             return render(
                 request, 'orders/order/created.html', {'order': order}
             )
