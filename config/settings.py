9c080dcba71ba7c28eb186e8392a00730c065132
Pietruszko <gabriel.karalus98@gmail.com>
2025-06-18 15:21:31 +0200

    fix: typo

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cart.apps.CartConfig',
-    'orders.app.OrdersConfig',
+    'orders.apps.OrdersConfig',
     'shop.apps.ShopConfig',
 ]
 
